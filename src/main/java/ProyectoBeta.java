ee50807df92ab910be0af5fdf79c1b121823363d
NandoProjectoFinal <61599328+NandoProjectoFinal@users.noreply.github.com>
2021-04-26 14:57:55 -0400

    dfghjk
    Signed-off-by: NandoProjectoFinal <61599328+NandoProjectoFinal@users.noreply.github.com>

--- src/main/java/ProyectoBeta.java
+++ src/main/java/ProyectoBeta.java
@@ -1,5 +1,51 @@
+import java.util.Scanner;
+
 public class ProyectoBeta {
     public static void main(String[] args) {
-
+        menu();
+        funcion();
     }
+
+    private static void funcion() {
+        ingresoDatos();
+        validacion(ingresoDatos());
+    }
+
+    private static String ingresoDatos() {
+        Scanner teclado = new Scanner(System.in);
+        String a = teclado.next();
+        return a;
+    }
+
+    private static boolean validacion(String ingresoDatos) {
+        boolean a = false;
+        switch (ingresoDatos){
+            case "1":
+                System.out.println("Lista de arboles");
+                a = true;
+                break;
+            case "2":
+                System.out.println("Lista de Lugares");
+                a = true;
+                break;
+            case "3":
+                System.out.println("Animales de la zona");
+                a= true;
+                break;
+            default:
+                System.out.println("opcion no valida");
+                a= false;
+                break;
+        }
+        return a;
+    }
+
+    private static void menu() {
+        System.out.println("[][][][][][][]][][][[][][][][][][][][][][][][][][][][][][]");
+        System.out.println("||||||||||||||Bienvenido a Guía del Perdido||||||||||||||");
+        System.out.println("[][][][][][][]][][][[][][][][][][][][][][][][][][][][][][]");
+        System.out.println("Desea ver " +"\n"+"[1] Lista de arboles" + "\n" + "[2] Lista de lugares turisticos"
+                + "\n" + "[3] Animales de la zona");
+    }
+
 }
